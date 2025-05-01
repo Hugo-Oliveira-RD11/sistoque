0125426de32f2ed56cf5613a4eed50a4b0650068
hugo <hugo.resennde1123@gmail.com>
2025-05-01 11:00:13 -0300

    feat(models): criado produtos

---
+++ src/backend/Models/Produtos.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models;
+
+public class Produto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int QuantidadeDisponivel { get; set; }
+
+    public string? Descricao { get; set; }
+    public DateTime? DataValidade { get; set; }
+}
