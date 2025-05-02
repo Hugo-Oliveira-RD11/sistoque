966341199cf922332c027d9d287f7dfe500412da
hugo <hugo.resennde1123@gmail.com>
2025-05-02 20:11:57 -0300

    feat(ClienteService): os Obter retornam null

--- src/backend/Services/Clientes/ClienteService.cs
+++ src/backend/Services/Clientes/ClienteService.cs
@@ -13,14 +13,14 @@ public class ClienteService : IClienteService
         _repository = repository;
     }
 
-    public async Task<IEnumerable<Cliente>> ObterTodosAsync()
+    public async Task<IEnumerable<Cliente>?> ObterTodosAsync()
         => await _repository.ObterTodosAsync();
 
-    public async Task<Cliente> ObterPorIdAsync(Guid id)
+    public async Task<Cliente?> ObterPorIdAsync(Guid id)
     {
-        var cliente = await _repository.ObterPorIdAsync(id);
+        var cliente = await _repository.ObterIdAsync(id);
         if (cliente is null)
-            throw new InvalidOperationException("Cliente não encontrado.");
+          return null;
 
         return cliente;
     }
@@ -31,6 +31,15 @@ public class ClienteService : IClienteService
         var resultado = await validador.ValidateAsync(cliente);
         if(!resultado.IsValid)
           throw new ArgumentException(resultado.Errors.First().ErrorMessage);
+
+        Cliente?  verificaClienteCpfCnpj = await  ObterPorCpfCnpjAsync(cliente.CpfCnpj!);
+        if(verificaClienteCpfCnpj != null)
+          throw new ArgumentException("Este cpf/cnpj ja foi cadastrado");
+
+        Cliente?  verificaClienteEmail = await ObterPorEmailAsync(cliente.Email!); // cirando regras de negocio
+        if(verificaClienteEmail != null)
+          throw new ArgumentException("Este email ja foi cadastrado");
+
 
         await _repository.AdicionarAsync(cliente);
     }
@@ -45,4 +54,22 @@ public class ClienteService : IClienteService
 
     public async Task RemoverAsync(Guid id)
         => await _repository.RemoverAsync(id);
+
+    public async Task<Cliente?> ObterPorEmailAsync(string email)
+    {
+      var cliente = await _repository.ObterEmailAsync(email);
+      if(cliente is null)
+        return null;
+
+      return cliente;
+    }
+
+    public async Task<Cliente?> ObterPorCpfCnpjAsync(string cpfCnpj)
+    {
+      var cliente = await _repository.ObterCpfCnpjAsync(cpfCnpj);
+      if(cliente is null)
+        return null;
+
+      return cliente;
+    }
 }
--- src/backend/Services/Clientes/IClienteService.cs
+++ src/backend/Services/Clientes/IClienteService.cs
@@ -4,8 +4,10 @@ namespace backend.Services.Clientes;
 
 public interface IClienteService
 {
-    Task<IEnumerable<Cliente>> ObterTodosAsync();
-    Task<Cliente> ObterPorIdAsync(Guid id);
+    Task<IEnumerable<Cliente>?> ObterTodosAsync();
+    Task<Cliente?> ObterPorIdAsync(Guid id);
+    Task<Cliente?> ObterPorEmailAsync(string email);
+    Task<Cliente?> ObterPorCpfCnpjAsync(string cpfCnpj);
     Task AdicionarAsync(Cliente cliente);
     Task AtualizarAsync(Cliente cliente);
     Task RemoverAsync(Guid id);
