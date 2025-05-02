49b125a819fe2e120031dc7f07b20c018338a92a
hugo <hugo.resennde1123@gmail.com>
2025-05-02 18:07:45 -0300

    feat(Models): removendo Endereco e consertando o nome da SenhaHash

--- src/backend/Models/Cliente.cs
+++ src/backend/Models/Cliente.cs
@@ -14,15 +14,13 @@ public class Cliente
     [Required]
     public string CpfCnpj { get; set; }
 
-    public string Endereco { get; set; }
-
     public string Telefone { get; set; }
 
     [Required]
     public string Email { get; set; }
 
     [Required]
-    public string senhaHash { get; set; }
+    public string SenhaHash { get; set; }
 
     [Required]
     public string Role { get; set; } // Ex: "Admin", "Usuario", etc.
