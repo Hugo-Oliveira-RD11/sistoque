7cdf3da64380f4f00055b6c0c9c0a2aff4356183
hugo <hugo.resennde1123@gmail.com>
2025-05-02 16:15:35 -0300

    feat(Repository): criando a base para o repository do cliente

---
+++ src/backend/Repository/Cliente/ClienteRepository.cs
@@ -0,0 +1,47 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repository;
+
+public class ClienteRepository : IClienteRepository
+{
+    private readonly AppDbContext _context;
+
+    public ClienteRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Cliente>> ObterTodosAsync()
+        => await _context.Clientes.ToListAsync();
+
+    public async Task<Cliente?> ObterPorIdAsync(Guid id)
+        => await _context.Clientes.FindAsync(id);
+
+    public async Task<Cliente?> ObterPorEmailAsync(string email)
+        => await _context.Clientes.FirstOrDefaultAsync(e => e.Email == email);
+
+    public async Task<Cliente?> ObterPorCpfCnpjAsync(string cpfCnpj)
+        => await _context.Clientes.FirstOrDefaultAsync(e => e.CpfCnpj == cpfCnpj);
+
+    public async Task AdicionarAsync(Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AtualizarAsync(Cliente cliente)
+    {
+        _context.Clientes.Update(cliente);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RemoverAsync(Guid id)
+    {
+        var cliente = await ObterPorIdAsync(id);
+        _context.Clientes.Remove(cliente!); // nao deve precisar de verificar se o cliente e nulo pois o service vai fazer isso
+        await _context.SaveChangesAsync();
+    }
+
+}
